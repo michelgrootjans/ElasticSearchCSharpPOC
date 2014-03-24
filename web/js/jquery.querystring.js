9386b9063c2e91153b6b175cb3dca2c0ae52219a
Michel Grootjans <michel.grootjans@gmail.com>
2014-03-24 09:16:17 +0100

    added querystring script

---
+++ web/js/jquery.querystring.js
@@ -0,0 +1,8 @@
+(function($){
+	$.extend({
+		querystring: function(name) {
+			var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+			return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+		}
+	});	
+})(jQuery);
