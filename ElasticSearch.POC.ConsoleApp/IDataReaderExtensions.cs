5c8e34c50678c467abd5d7222992ed7b55c7c6cd
Michel Grootjans <michel.grootjans@gmail.com>
2014-03-31 08:24:31 +0200

    cleanup

--- ElasticSearch.POC.ConsoleApp/IDataReaderExtensions.cs
+++ ElasticSearch.POC.ConsoleApp/IDataReaderExtensions.cs
@@ -12,13 +12,6 @@ namespace ElasticSearch.POC.ConsoleApp
             return (Guid)value;
         }
 
-//        public static short GetShortValue(this IDataReader reader, string key)
-//        {
-//            var value = reader[key];
-//            if (value is DBNull) return 0;
-//            return (short)value;
-//        }
-
         public static int GetIntValue(this IDataReader reader, string key)
         {
             var value = reader[key];
@@ -32,14 +25,6 @@ namespace ElasticSearch.POC.ConsoleApp
             if (value is DBNull) return false;
             return (bool)value;
         }
-
-//        public static string GetDateValue(this IDataReader reader, string key, string format = "dd/MM/yyyy")
-//        {
-//            var value = reader[key];
-//            if (value is DBNull) return "";
-
-//            return ((DateTime)value).ToString(format);
-//        }
 
         public static string GetStringValue(this IDataReader reader, string key)
         {
