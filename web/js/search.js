905141af36394e9ba06c961b82fd3035d1b3a2b3
Michel Grootjans <michel.grootjans@gmail.com>
2014-06-01 18:25:31 +0200

    moved title to html file

--- web/js/search.js
+++ web/js/search.js
@@ -1,13 +1,13 @@
 function execute_search(client){
   var q =  escapeHtml($.querystring('q'));
   if(q=="null") return;
+
+  // var results = $('<div/>').addClass("search-results");
+  // var search_resluts_title = $('<h3/>').append("Searching ...");
+  $('#search-results .title').html('Searching ...');
+
   var perPage = parseInt($.querystring('per_page') || 10);
   var pageNum = parseInt($.querystring('page') || 0);
-
-  var results = $('<div/>').addClass("search-results");
-  var search_resluts_title = $('<h3/>').append("Searching ...");
-
-  $('#search_results').html(search_resluts_title).append(results);
 
   client.search(
   {
@@ -44,9 +44,9 @@ function execute_search(client){
   })
   .then(function(body) {
     console.log(body);
-    search_resluts_title.html('Search Results (' + body.hits.total + ')');
+    $('#search-results .title').html('Search Results (' + body.hits.total + ')');
     body.hits.hits.forEach(function(item){
-      results.append(display_result(item));
+      $('#search-results').append(display_result(item));
     });
     $('#facets').append(display_facets(body.facets));
 
