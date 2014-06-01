ca2fba4df4f5ec891b74fad1a64f777bc3bcdff6
Michel Grootjans <michel.grootjans@gmail.com>
2014-06-01 21:23:20 +0200

    cleanup query

--- ElasticSearch.POC.ConsoleApp/DataAccessLayer.cs
+++ ElasticSearch.POC.ConsoleApp/DataAccessLayer.cs
@@ -33,11 +33,11 @@ namespace ElasticSearch.POC.ConsoleApp
 	                   pj.Identificatie,
 	                   pj.Omschrijving,
 	                   pj.projectType,
-	                   ps.Naam as 'status',
+	                   st.Naam as 'status',
 	                   g.Naam as 'gemeente'
-                from VMSW_PO_Projecten pj
-                left outer join VMSW_PO_ProjectStatussen ps on ps.Id = pj.StatusID
-                left outer join VMSW_GIPR_Gemeentes g on pj.GemeenteID = g.ID
+                from Projecten pj
+                left outer join Statussen st on st.Id = pj.StatusID
+                left outer join Gemeentes g on pj.GemeenteID = g.ID
                 ";
         private static Project MapToProject(IDataReader reader)
         {
