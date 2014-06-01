c289ec7ee5b5321dd5303b06096b1c74ed3c68fb
Michel Grootjans <michel.grootjans@gmail.com>
2014-06-01 15:28:06 +0200

    extracted filter.js

--- web/js/search.js
+++ web/js/search.js
@@ -55,33 +55,3 @@ function execute_search(client){
     notify('error', error.message);
   });
 }
-
-
-function getFilter(){
-  //this is the place where selected facets will be filtered from the hits
-  var result = [];
-  getFacetFilter(result, 'project_type');
-  getFacetFilter(result, 'programmatie');
-  getFacetFilter(result, 'status');
-  getFacetFilter(result, 'category');
-  getFacetFilter(result, 'user.name');
-  if (result.length == 0)
-    result.push({});
-  return result;
-}
-
-function getFacetFilter(result, facet_field){
-  var value = $.querystring(facet_field);
-  if(value == null) return;
-
-  var filter = { term: {}};
-  filter.term[facet_field] = value
-  result.push(filter);
-}
-
-
-
-
-
-
-
